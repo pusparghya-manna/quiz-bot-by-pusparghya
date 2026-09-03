842811152e161bd91df097c2154c1389d18c02d4
Pusparghya Manna <pusparghyamanna26@gmail.com>
2026-09-03 20:16:30 +0000

    Add official multicolored Google icon for login button

--- frontend/src/icons.tsx
+++ frontend/src/icons.tsx
@@ -58,3 +58,13 @@ export const IconExternal = ({ className }: P) => <svg {...s} className={classNa
 export const IconShuffle = ({ className }: P) => <svg {...s} className={className}><path d="M16 3h5v5"/><path d="M4 20 21 3"/><path d="M21 16v5h-5"/><path d="M15 15l6 6"/><path d="M4 4l5 5"/></svg>;
 export const IconMinus = ({ className }: P) => <svg {...s} className={className}><path d="M5 12h14"/></svg>;
 export const IconBookmark = ({ className }: P) => <svg {...s} className={className}><path d="M19 21l-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"/></svg>;
+
+/** Official multicolored Google "G" logo (matches design mock) */
+export const IconGoogle = ({ className }: P) => (
+  <svg className={className} viewBox="0 0 24 24" width="1em" height="1em" aria-hidden="true">
+    <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
+    <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" />
+    <path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" />
+    <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" />
+  </svg>
+);
